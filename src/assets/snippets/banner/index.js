1222a5465ebc0cf1626513c6173325de90277440
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-03-07 22:57:13 -0300

    Banner

--- src/assets/snippets/banner/index.js
+++ src/assets/snippets/banner/index.js
@@ -5,6 +5,15 @@ define(['jquery', 'slick-carousel'], ($) => {
         autoplay: true,
         autoplaySpeed: 5000,
         nextArrow: '<button class="slick-next" type="button"><i class="fas fa-chevron-right"></i></button>',
-        prevArrow: '<button class="slick-prev" type="button"><i class="fas fa-chevron-left"></i></button>'
+        prevArrow: '<button class="slick-prev" type="button"><i class="fas fa-chevron-left"></i></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
     });
 }); 
