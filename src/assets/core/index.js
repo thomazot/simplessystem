f4bf92c075328aa9bc1c4d0071dcfab06e5eb122
Thomaz T. O. Toyama <thomazot@gmail.com>
2019-03-19 00:00:36 -0300

    Ajustes animation

--- src/assets/core/index.js
+++ src/assets/core/index.js
@@ -1,17 +1,19 @@
 import './reset.styl';
 
 
-define(['scrollmagic'], (ScrollMagic) => {
+define(['scrollmagic', 'jquery'], (ScrollMagic, $) => {
     // init controller
     const controller = new ScrollMagic.Controller({
         // container: '#container'
     });
 
     const efects = ['solutions', 'products', 'codificadores', 'suprimentos', 'contact', 'blog'];
+    let index = 1;
     efects.forEach((scene) => {
         // create a scene
         const box = document.querySelector(`#${ scene }`);
         if(box) {
+            $(box).css({ 'position': 'relative', 'z-index': index++  });
             box.classList.add('scrollmagic');
 
             const sceneMagic = new ScrollMagic.Scene({
